f0ac23ba9c5fd6fadfc91041aa38fe45e9207053
fatima BENAZZOU <b.fati2102@gmail.com>
2025-04-07 23:17:25 +0100

    update

--- src/components/modals/EditProductModal.tsx
+++ src/components/modals/EditProductModal.tsx
@@ -1,33 +1,33 @@
-// /components/EditProductModal.tsx
-"use client";
+// // /components/EditProductModal.tsx
+// "use client";
 
-import { useState } from "react";
-import EditProductForm from "../EditProductForm";
+// import { useState } from "react";
+// // import EditProductForm from "../EditProductForm";
 
-interface EditProductModalProps {
-    product: ProductI;
-}
+// interface EditProductModalProps {
+//     product: ProductI;
+// }
 
-export default function EditProductModal({ product }: EditProductModalProps) {
-    const [isOpen, setIsOpen] = useState(false);
+// export default function EditProductModal({ product }: EditProductModalProps) {
+//     const [isOpen, setIsOpen] = useState(false);
 
-    return (
-        <>
-            <button className="btn btn-sm btn-info" onClick={() => setIsOpen(true)}>
-                Edit
-            </button>
+//     return (
+//         <>
+//             <button className="btn btn-sm btn-info" onClick={() => setIsOpen(true)}>
+//                 Edit
+//             </button>
 
-            <dialog open={isOpen} className="modal">
-                <div className="modal-box">
-                    <h3 className="font-bold text-lg">Edit Product</h3>
+//             <dialog open={isOpen} className="modal">
+//                 <div className="modal-box">
+//                     <h3 className="font-bold text-lg">Edit Product</h3>
 
-                    <div className="modal-action">
-                        <button t onClick={() => setIsOpen(false)} className="btn">
-                            Close
-                        </button>
-                    </div>
-                </div>
-            </dialog>
-        </>
-    );
-}
+//                     <div className="modal-action">
+//                         <button t onClick={() => setIsOpen(false)} className="btn">
+//                             Close
+//                         </button>
+//                     </div>
+//                 </div>
+//             </dialog>
+//         </>
+//     );
+// }
